9591a5560724ec1407b9410b2ec9f636970aa718
squidmin <morse.james.r@gmail.com>
2023-05-07 16:26:49 -0500

    Edit: code formatting (cont'd)

--- 01_basic_constructs_and_elementary_data_types/05_basic_string_operations/basic_string_operations.go
+++ 01_basic_constructs_and_elementary_data_types/05_basic_string_operations/basic_string_operations.go
@@ -2,6 +2,10 @@ package main
 
 import "fmt"
 
+func main() {
+	stringOperations()
+}
+
 func stringOperations() {
 	fmt.Println(len("Hello World")) // len() usage
 	fmt.Println("Hello World"[1])   // String indexing

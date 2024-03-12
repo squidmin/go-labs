f00800537f3643079b9a4c3c9c83d185ddab0677
squidmin <morse.james.r@gmail.com>
2024-03-12 16:35:39 -0500

    Cleanup main func in practice file

--- practice/04_arrays_and_slices/01_declaration_and_initialization/arrays/arrays.go
+++ practice/04_arrays_and_slices/01_declaration_and_initialization/arrays/arrays.go
@@ -1,14 +1,6 @@
 package main
 
-import "fmt"
-
 // Arrays
 func main() {
-	var arr1 [5]int
-	for i := 0; i < len(arr1); i++ {
-		arr1[i] = i * 2
-	}
-	for i, value := range arr1 {
-		fmt.Printf("Value at index %d is %d\n", i, value)
-	}
+
 }
